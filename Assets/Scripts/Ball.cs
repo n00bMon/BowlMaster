fe9e3fd024e7e74b5825d0bf4fbff237c477ca86
Bharat <bharat.monavarthi@hotmail.com>
2017-10-16 23:23:21 +0530

    Added a script to the ball adding velocity and an audio clip

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float velocity;
+
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = new Vector3(0, 0, velocity);
+
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        audioSource.Play();
+    }
+}
